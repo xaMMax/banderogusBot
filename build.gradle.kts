721e31945e27ff191facae9cd9b65d6e97c62a71
xaMMax <petrolijuice@gmail.com>
2023-08-05 22:21:03 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.telegram/telegrambots
+    implementation("org.telegram:telegrambots:6.3.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
